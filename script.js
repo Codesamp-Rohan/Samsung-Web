a19978fed37b73e1f205fe276673321a60e22200
DocYard <rohanchaudhary.bkbiet2022@gmail.com>
2023-04-05 15:16:05 +0530

    1

---
+++ script.js
@@ -0,0 +1,48 @@
+function hoverdown_shop(){
+    document.getElementById("menu_container_shop").style.top = "80px";
+}
+function hoverup_shop(){
+    document.getElementById("menu_container_shop").style.top = "-270px";
+}
+
+function hoverdown_mobile(){
+    document.getElementById("menu_container_mobile").style.top = "80px";
+}
+function hoverup_mobile(){
+    document.getElementById("menu_container_mobile").style.top = "-270px";
+}
+
+function hoverdown_tvav(){
+    document.getElementById("menu_container_tvav").style.top = "80px";
+}
+function hoverup_tvav(){
+    document.getElementById("menu_container_tvav").style.top = "-270px";
+}
+
+function hoverdown_ha(){
+    document.getElementById("menu_container_ha").style.top = "80px";
+}
+function hoverup_ha(){
+    document.getElementById("menu_container_ha").style.top = "-270px";
+}
+
+function hoverdown_lm(){
+    document.getElementById("menu_container_lm").style.top = "80px";
+}
+function hoverup_lm(){
+    document.getElementById("menu_container_lm").style.top = "-270px";
+}
+
+function hoverdown_display(){
+    document.getElementById("menu_container_display").style.top = "80px";
+}
+function hoverup_display(){
+    document.getElementById("menu_container_display").style.top = "-270px";
+}
+
+function hoverdown_st(){
+    document.getElementById("menu_container_st").style.top = "80px";
+}
+function hoverup_st(){
+    document.getElementById("menu_container_st").style.top = "-270px";
+}
